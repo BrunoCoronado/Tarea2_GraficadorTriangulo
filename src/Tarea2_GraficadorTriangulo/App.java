b392dec14c8534f8c4adab602f4ab60e880c437e
BrunoCoronado <bcoronado.morales@gmail.com>
2018-02-19 19:13:25 -0600

    Agragando comentarios y cambio en un ciclo
    agregando comentarios al codigo y sacando una variable de un ciclo

--- src/Tarea2_GraficadorTriangulo/App.java
+++ src/Tarea2_GraficadorTriangulo/App.java
@@ -12,54 +12,65 @@ import java.util.Scanner;
  * @author bruno
  */
 public class App {
+    /*Obtener el numero y analizarlo*/
     public void IngresoDatos()
     {
         Scanner scanner = new Scanner(System.in);          
         int NoLineas=0;
         
         System.out.println("Ingrese el numero de lineas:      NUMERO 0 PARA SALIR");
-             
-            try {
-                NoLineas = scanner.nextInt();
-                if(NoLineas<0){
-                    System.out.println("SOLO NUMEROS POSITIVOS");
-                    IngresoDatos();
-                }
-                switch(NoLineas){
-                    case 0:
-                        System.exit(0);
-                    default:                        
-                        Graficar(NoLineas);
-                }                       
-            } catch (Exception e) {
-                System.out.println("Ingrese solo numeros");
+            
+        //Try/Catch para controlar el ingreso de caracteres no permitidos
+        try {
+            
+            //Obtener el numero
+            NoLineas = scanner.nextInt();
+            if(NoLineas<0){
+                System.out.println("SOLO NUMEROS POSITIVOS");
+                //Recursividad para volver a pedir el ingreso de datos
                 IngresoDatos();
             }
+            switch(NoLineas){
+                case 0:
+                    //salir del sistema, segun la opcion elegida por el usuario
+                    System.exit(0);
+                default:                        
+                    Graficar(NoLineas);
+            }                       
+            //controlando caracteres no permitidos
+        } catch (Exception e) {
+            System.out.println("Ingrese solo numeros");
+            //Recursividad para volver a pedir el ingreso de datos
+            IngresoDatos();
+        }
     }
 
     private void Graficar(int noLineas) {
-        
-        StringBuffer lineas = new StringBuffer();
+       /*tipo de dato que tiene los metodos append, delete e insert para agregar, eliminar o insetar caracteres
+       en ciertas posiciones dentro de la linea de caracteres*/
+       StringBuffer lineas = new StringBuffer();
         
        lineas.append("/");
 
         boolean espacioInicial = true;
-
+        
+        //Ciclo para imprimir las lineas de caracteres
         for(int contador=0;contador<noLineas;contador++)
         {
+            //ciclo para dejar espacio del borde
             while (espacioInicial) 
             {                
                 for(int contadorEspacio=0;contadorEspacio<noLineas;contadorEspacio++){
                     lineas.insert(0," ");
-                    espacioInicial=false;
-                }
+                }                
+                espacioInicial=false;
             }     
-
+            //imprimir las lineas de caracteres
             System.out.println(lineas+"\\");  
             lineas.append("/\\");
             lineas.delete(0,1);            
         }
-        
+        //recursividad para volver a iniciar el programa
         IngresoDatos();
     }
 }
--- src/Tarea2_GraficadorTriangulo/Main.java
+++ src/Tarea2_GraficadorTriangulo/Main.java
@@ -15,7 +15,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // instanciamos e iniciamos el programa
         App app = new App();
         app.IngresoDatos();
     }
